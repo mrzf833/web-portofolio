656965235cafcbdb5b9ace2c90495a6933a1789a
falaqzulfa <palaktais@gmail.com>
2025-03-18 11:22:36 +0700

    add file vercel

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,8 +39,8 @@ export default {
   build: {
   },
 
-  server: {
-    port: 3000, // default: 3000
-    host: "0.0.0.0"
-  }
+  // server: {
+  //   port: 3000, // default: 3000
+  //   host: "0.0.0.0"
+  // }
 }
